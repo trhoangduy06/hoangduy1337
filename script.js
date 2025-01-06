2680ebcfbad843741d465999acec337032d7e3c1
trhoangduy06 <tranhoangduysz@hotmail.com>
2025-01-07 00:46:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// Đóng thông báo khi người dùng nhấn nút "×"
+// Đóng thông báo UI
 function closeNotification() {
     const notification = document.getElementById("ui-notification");
     notification.classList.add("hidden");
+}
+
+// Chuyển đổi sang form đăng ký
+function toggleRegister() {
+    document.getElementById("login-section").style.display = "none";
+    document.getElementById("register-section").style.display = "block";
+}
+
+// Chuyển đổi sang form đăng nhập
+function toggleLogin() {
+    document.getElementById("register-section").style.display = "none";
+    document.getElementById("login-section").style.display = "block";
 }
